3e6303c3d3c778e9b263a50fd7146194ee266326
dimohy <58620778+dimohy@users.noreply.github.com>
2024-01-11 15:36:55 +0900

    문제4

---
+++ 2023/Day04.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+
+namespace _2023;
+
+public class Day04 : ISolve
+{
+    public static string Solve1(string input, params object[] args)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            var card = Card.Parse(line);
+            sum += card.GetScore();
+        }
+        
+        return sum.ToString();
+    }
+
+    public static string Solve2(string input, params object[] args)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var cards = lines.Select(Card.Parse).ToArray();
+
+        var count = cards.Select((x, i) => (x, i)) .Sum(x => GetCardCount(cards, x.i));
+        return count.ToString();
+
+        static int GetCardCount(Card[] cards, int index)
+        {
+            var card = cards[index];
+            var count = card.GetMatchCount();
+            if (count is 0)
+                return 1;
+
+            var sum = 1;
+            for (var i = index + 1; i < index + 1 + count; i++)
+            {
+                var result = GetCardCount(cards, i);
+                sum += result;
+            }
+
+            return sum;
+        }
+    }
+
+    class Card(string name, int count, int score)
+    {
+        public string Name => name;
+
+        public static Card Parse(string line)
+        {
+            var parts = line.Split(":");
+            var name = parts[0].Trim();
+            var numbers = parts[1].Split("|");
+            var numbers1 = numbers[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var numbers2 = numbers[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var count = numbers2.Count(x => numbers1.Contains(x));
+            var score = (int)Math.Pow(2, count - 1);
+
+            return new Card(name, count, score);
+        }
+
+        public int GetScore() => score;
+        public int GetMatchCount() => count;
+    }
+}
--- UnitTests/2023Test.cs
+++ UnitTests/2023Test.cs
@@ -85,4 +85,50 @@ internal class _2023Test
         var result = Day03.Solve1(input);
         Assert.That(result, Is.EqualTo("4361"));
     }
+
+    [TestCase("""
+                467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..
+                """)]
+    public void Day3_Test2(string input)
+    {
+        var result = Day03.Solve2(input);
+        Assert.That(result, Is.EqualTo("467835"));
+    }
+
+    [TestCase("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """)]
+    public void Day4_Test1(string input)
+    {
+        var result = Day04.Solve1(input);
+        Assert.That(result, Is.EqualTo("13"));
+    }
+
+    [TestCase("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """)]
+    public void Day4_Test2(string input)
+    {
+        var result = Day04.Solve2(input);
+        Assert.That(result, Is.EqualTo("30"));
+    }
 }
